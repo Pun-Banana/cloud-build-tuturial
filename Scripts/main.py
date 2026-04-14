46656b0f13d674015862a39ac4b9f9931817ee92
Punyaporn O. <chocolate.banana@hotmail.co.th>
2026-04-15 00:28:24 +0700

    Edit main.py

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,12 +1,11 @@
-import os
 from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/message")
+@app.route("/message", methods=["GET", "POST"])
 def hello_world():
     return f"Hello! Your Python service is live. In Version 2"
 
 if __name__ == "__main__":
     # Use the port assigned by Cloud Run or default to 8080
-    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 8080)))
+    app.run(debug=True, host="0.0.0.0", port=8080)
