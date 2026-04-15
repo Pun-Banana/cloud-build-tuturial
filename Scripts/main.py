c7064a902054d36c36995b9561dd1513ab9be131
Punyaporn O. <chocolate.banana@hotmail.co.th>
2026-04-15 23:09:29 +0700

    Edit main.py to add new path

--- Scripts/main.py
+++ Scripts/main.py
@@ -2,4 +2,9 @@ import functions_framework
 
 @functions_framework.http
 def message(request):
-    return "Hello! This is message test version 3!\n\n"
+    path = request.path
+
+    if path == '/message':
+        return "Hello! This is message test version 3!\n\n", 200
+    else:
+        return f"Error: The path {path} is invalid.", 404
